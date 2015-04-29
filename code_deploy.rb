837be15604b1fd162ed380a9b12a310fe75c0bbb
Matt Jones <mattjones@yieldbot.com>
2015-04-29 15:28:46 -0400

    debugging

--- code_deploy.rb
+++ code_deploy.rb
@@ -18,6 +18,7 @@ require 'fileutils'
 # Build a gem and deploy it to rubygems
 #
 def deploy_rubygems(spec, plugin)
+  puts " this is the version string { SensuPluginsApache::Version::VER_STRING }"
   puts "this is the deploy gem stuff"
   puts `rm *.gem`
   puts `gem build #{ plugin }.gemspec`
